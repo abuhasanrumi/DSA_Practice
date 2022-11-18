00605bbe8e76624fa09589a8aaf668f3c3bfed40
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2022-11-18 23:21:20 +0600

    binary search

--- searching.js
+++ searching.js
@@ -1,3 +1,5 @@
+// Linear Searching
+
 let arr = [12, 43, 26, 74, 29, 41, 58, 91, 10];
 let target = 74;
 
@@ -11,3 +13,22 @@ function linearSearch(arr, target) {
 }
 
 console.log(linearSearch(arr, target));
+
+
+// Binary Searching
+let arr2 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+function binarySearch(arr, target) {
+    let start = 0;
+    let end = arr.length - 1
+    let mid = Math.floor((start + end) / 2);
+
+    while (true) {
+        if (mid === target) {
+            return "found"
+        } else if (mid < target) {
+            start = mid + 1;
+
+        }
+    }
+}
