734152d9554663fda7daacacd478428af5605a76
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2022-11-23 23:09:41 +0600

    matching number problem solved

--- searching.js
+++ searching.js
@@ -41,45 +41,77 @@
 
 
 
-//Binary Searching Asc-Dsc
+// //Binary Searching Order Agnostic
 
+// function binarySearch(arr, target) {
+//     let start = 0;
+//     let end = arr.length - 1
+//     let mid;
+//     let asc;
+
+//     if (arr[start] <= arr[end]) {
+//         asc = true
+//     } else {
+//         asc = false;
+//     }
+
+//     while (start <= end) {
+//         mid = Math.floor((start + end) / 2);
+
+//         if (arr[mid] === target) {
+//             return "Found at index " + mid;
+//         } else if (asc) {
+//             if (arr[mid] < target) {
+//                 start = mid + 1;
+//                 mid = Math.floor((start + end) / 2);
+//             } else {
+//                 end = mid - 1;
+//                 mid = Math.floor((start + end) / 2);
+//             }
+//         } else if (!asc) {
+//             console.log("here")
+//             if (arr[mid] > target) {
+//                 start = mid - 1;
+//                 mid = Math.floor((start + end) / 2);
+//             } else {
+//                 end = mid + 1;
+//                 mid = Math.floor((start + end) / 2);
+//             }
+//         }
+//     }
+// }
+
+// console.log(binarySearch([90, 80, 70, 55, 40, 20], 55))
+// console.log(binarySearch([10, 20, 30, 40, 50, 60], 50))
+
+
+
+// Matching Number Binary Search
 function binarySearch(arr, target) {
     let start = 0;
     let end = arr.length - 1
-    let mid;
-    let asc;
-
-    if (arr[start] <= arr[end]) {
-        asc = true
-    } else {
-        asc = false;
-    }
+    let mid = Math.floor((start + end) / 2);
 
     while (start <= end) {
-        mid = Math.floor((start + end) / 2);
-
+        let counter = 0;
         if (arr[mid] === target) {
-            return "Found at index " + mid;
-        } else if (asc) {
-            if (arr[mid] < target) {
-                start = mid + 1;
-                mid = Math.floor((start + end) / 2);
-            } else {
-                end = mid - 1;
-                mid = Math.floor((start + end) / 2);
-            }
-        } else if (!asc) {
-            console.log("here")
-            if (arr[mid] > target) {
-                start = mid - 1;
-                mid = Math.floor((start + end) / 2);
-            } else {
-                end = mid + 1;
-                mid = Math.floor((start + end) / 2);
+            counter++;
+        }
+
+        for (let i = mid - 1; i >= 0; i--) {
+            if (arr[i] === target) {
+                counter++;
             }
         }
+
+        for (let i = mid + 1; i < arr.length; i++) {
+            if (arr[i] === target) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
 
-console.log(binarySearch([90, 80, 70, 55, 40, 20], 55))
-// console.log(binarySearch([10, 20, 30, 40, 50, 60], 50))
+console.log(binarySearch([90, 80, 80, 70, 55, 40, 20], 100))
+// console.log(binarySearch([10, 20, 30, 40, 40, 40, 40, 40, 50, 60], 20))
